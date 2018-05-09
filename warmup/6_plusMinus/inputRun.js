076e590c0b3379dd0822130d023f322d92dc2e59
insanjati <prasetyainsan@gmail.com>
2018-05-09 13:02:48 +0700

    add warmup/plusMinus

---
+++ warmup/6_plusMinus/inputRun.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function plusMinus(arr) {
+    let pos = 0, neg = 0, zero = 0;
+    for (let i=0; i<arr.length; i++)   {
+        if (arr[i] > 0) {
+        pos++;
+        } else if (arr[i] < 0) {
+        neg++;
+        } else {
+        zero++;
+        }
+    }
+    console.log(pos / arr.length);
+    console.log(neg / arr.length);
+    console.log(zero / arr.length);
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    plusMinus(arr);
+}
